e72ec6b18b60eddb5a51b09b5290ba068284fa45
HadBoard <hadboard@hotmail.com>
2021-10-29 01:34:22 +0330

    final change

--- HM2/tcp.py
+++ HM2/tcp.py
@@ -1,14 +1,37 @@
 import socket
+import os.path
+
+print("\nSeyed Hadi Ranjbar :)\n")
+
+host = input("Enter your domain : ")
+page = input("Enter target page : ")
 
 s=socket.socket()
-host=socket.gethostbyname('www.google.com')
+host=socket.gethostbyname(host)
 port=80
+request = "GET /hadi.php \r\n";
+request = "GET /" + page + " HTTP/1.0\r\n\r\n"
 
 s.connect((host,port))
-s.sendall(bytes("GET /\r\n", "utf-8"))
+s.sendall(request.encode())
 
-response = s.recv(10000)
+response = s.recv(10000).decode()
 
-val = response.split(bytes("\r\n\r\n", "utf-8"))
+val = response.split("\r\n")
 
-print(response)
+###############################################################
+
+if '200' in val[0] :
+
+    print("HTTP Response Code : 200")
+    file = input("input file name (for save) : ")
+    path = os.path.dirname(__file__)
+    file = open(os.path.join(path, "tcp/" + file + ".txt"),"w", encoding='utf-8')
+    file.write(val[14])
+    file.close()    
+
+elif '404' in val[0] :
+    print("HTTP Response Code : 404")
+else :
+    print(val[0])
+
--- HM2/url.py
+++ HM2/url.py
@@ -1,7 +1,29 @@
 import urllib.request
+import os.path
 
-html_res = urllib.request.urlopen('http://www.python.org/')
+print("\nSeyed Hadi Ranjbar :)\n")
+
+links = [
+    "https://stackoverflow.com/"
+    "https://github.com/" ,
+    "http://www.python.org/" ,
+    "https://www.educba.com/" ,
+    "https://extensions.gnome.org/" ,
+    "https://wordpress.org/" ,
+    "https://cpanel.net/" ,
+    "https://cafebazaar.ir/" ,
+    "https://myket.ir",
+    "https://basalam.com/"
+]
+
+url = links[0]
+
+html_res = urllib.request.urlopen(url)
 html_content = html_res.read()
-file = open("server/www.python.org.html", 'w')
+url = url.replace("https://", "")
+url = url.replace("http://", "")
+url = url.replace("/", "")
+path = os.path.dirname(__file__)
+file = open(os.path.join(path, "server/" + url + ".html"),"w", encoding='utf-8')
 file.write(html_content.decode())
 file.close()    
